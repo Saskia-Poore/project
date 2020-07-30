78c90e1b01e857a693584bc03c2c1f8dcd50fbf7
Saskia Poore <saskia@Saskias-MacBook-Pro.local>
2020-07-30 23:43:09 +0100

    eleventh commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,39 @@
-var firstvar = 74;
-console.log("my variable is " + firstvar);
+function checkIt() {
+    var error = 0;
+
+    if (myform.myname.value == "") {
+        alert("Please enter a name");
+        document.getElementById("myname").style.background = "red";
+        error = 1
+    }
+
+    if (myform.myemail.value == "") {
+        alert("Please enter an email address");
+        document.getElementById("myemail").style.background = "red";
+        error = 1
+    }
+
+    if (myform.myphonenum.value == "") {
+        alert("Please enter a phone number");
+        document.getElementById("myphonenum").style.background = "red";
+        error = 1
+    }
+
+    if (myform.myaddress.value == "") {
+        alert("Please enter an address");
+        document.getElementById("myaddress").style.background = "red";
+        error = 1
+    }
+
+    if (error == 0) {
+        alert("The form will be sent");
+        myform.submit()
+    }
+
+}
+
+function resetcolor() {
+    document.getElementById("myname").style.background = "white";
+    document.getElementById("myaddress").style.background = "white";
+}
+        
